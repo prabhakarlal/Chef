b5034e5a6882e24476f5feb7689ab8808ccbac20
”Prabhakarlal” <prabhakarlal20@hotmail.com>
2018-02-06 14:34:32 +0000

    Chef-repo Second Push of Sree

---
+++ cookbooks/sree/README.md
@@ -0,0 +1,4 @@
+# sree
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/sree/recipes/default.rb
@@ -0,0 +1,8 @@
+file '/root/Chef/first.txt' do
+content 'This is first chef test Push'
+end
+
+# Cookbook:: sree
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
